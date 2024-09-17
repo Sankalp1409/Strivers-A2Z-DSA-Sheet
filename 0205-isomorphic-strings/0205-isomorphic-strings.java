7e73f6d31193f704ec37196902cb69143d9d41a5
Sankalp1409 <115624402+Sankalp1409@users.noreply.github.com>
2024-09-18 00:38:19 +0530

    Time: 10 ms (67.33%), Space: 42.7 MB (12.28%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s.length()!=t.length())
+            return false;
+        Map<Character,Character>mp=new HashMap<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+            
+            if(mp.containsKey(ch1))
+            {
+                if(ch2!=mp.get(ch1))
+                    return false;
+            }
+            else
+            {
+                if(mp.containsValue(ch2))
+                    return false;
+                else
+                    mp.put(ch1,ch2);
+            }
+        }
+        return true;
+    }
+}
