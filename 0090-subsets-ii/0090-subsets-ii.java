d26de93df54cd21d6806fc525576bd979e85b918
Sankalp1409 <115624402+Sankalp1409@users.noreply.github.com>
2024-09-24 22:15:04 +0530

    Time: 1 ms (99.93%), Space: 43.4 MB (49.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,28 +1,25 @@
 class Solution {
     
-    public void solve(int []nums, int idx,List<Integer>temp,List<List<Integer>>ansList)
+    public void solve(int []nums, int idx, List<Integer>temp,List<List<Integer>>ansList)
     {
-        if(idx==nums.length)
+        ansList.add(new ArrayList<>(temp));
+        
+        for(int i=idx;i<nums.length;i++)
         {
-            if(!ansList.contains(temp))
-                ansList.add(new ArrayList<>(temp));
-            return ;
+            if(i!=idx && nums[i]==nums[i-1])
+                continue;
+            temp.add(nums[i]);
+            solve(nums,i+1,temp,ansList);
+            temp.remove(temp.size()-1);
         }
-        
-        // notTake
-        solve(nums,idx+1,temp,ansList);
-        
-        // take
-        temp.add(nums[idx]);
-        solve(nums,idx+1,temp,ansList);
-        temp.remove(temp.size()-1);
     }
     
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         
         Arrays.sort(nums);
-        List<List<Integer>>ansList=new ArrayList<>();
         List<Integer>temp=new ArrayList<>();
+        List<List<Integer>>ansList=new ArrayList<>();
+        
         solve(nums,0,temp,ansList);
         return ansList;
     }
