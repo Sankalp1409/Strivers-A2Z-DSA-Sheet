dab39aec3ed9cbbcf35213088a5594ac32cd808d
Sankalp Shrivastava <115624402+Sankalp1409@users.noreply.github.com>
2024-12-12 00:04:54 +0530

    Time: 91 ms (15.93%), Space: 60.6 MB (28.22%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,31 +1,33 @@
 class Solution {
-    class pair{
-        int start,end;
+    
+    public int binarySearch(int []nums, int key)
+    {
+        int low=0,high=nums.length-1;
         
-        public pair(int start, int end)
+        while(low<=high)
         {
-            this.start=start;
-            this.end=end;
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]<=key)
+                low=mid+1;
+            else
+                high=mid-1;
         }
+        return high;
     }
     
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
-        ArrayList<pair>list=new ArrayList<>();
+        int result=1;
         for(int i=0;i<nums.length;i++)
-            list.add(new pair(nums[i]-k,nums[i]+k));
-        
-        int result=0;
-        Deque<Integer>dq=new ArrayDeque<>();
-        for(int i=0;i<list.size();i++)
         {
-            while(!dq.isEmpty() && dq.getFirst()<list.get(i).start)
-                dq.pollFirst();
-            dq.add(list.get(i).end);
-            result=Math.max(result,dq.size());
+            int x=nums[i];
+            int y=x+2*k;
+            
+            int j=binarySearch(nums,y);
+            
+            result=Math.max(result,j-i+1);
         }
         return result;
-        
     }
-    
 }
