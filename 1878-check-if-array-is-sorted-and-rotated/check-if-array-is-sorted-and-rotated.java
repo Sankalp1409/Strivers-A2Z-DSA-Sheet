e3cae1910b155e6fb8a291cf5c3d93b9507e9be8
Sankalp Shrivastava <115624402+Sankalp1409@users.noreply.github.com>
2025-06-19 00:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (39.36%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -4,9 +4,9 @@ class Solution {
         int breakPoint=0;
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i] > nums[(i+1)%nums.length])
+            if(nums[i]>nums[(i+1)%nums.length])
                 breakPoint++;
         }
-        return breakPoint<=1;
+        return breakPoint<2;
     }
 }
